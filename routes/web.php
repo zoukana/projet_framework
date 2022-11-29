eedd49d6a38eae1cb05b5bc529ec7f24a5250cfc
oumytoure <toureoumy12@gmail.com>
2022-11-29 18:55:18 +0000

    ajout

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*  Route::get('/connecxion', function () {
+    return view('connecxion');
+}); */ 
+
+ Route::get('/connecxion', 'ConnecxionController@formulaire');
+Route::post('/connecxion', 'ConnecxionController@traitement');
