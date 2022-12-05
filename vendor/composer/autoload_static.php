49c1c66fda452fbebd3528ef5dbf5bcebf18043d
zoukana <assanediallo192@gmail.com>
2022-12-05 13:17:19 +0000

    ok

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -347,8 +347,8 @@ class ComposerStaticInit2542954053ea1b05776412a270daa52f
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
